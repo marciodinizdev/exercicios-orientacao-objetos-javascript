b10c426205f8008d2f972c02db64daf859d49598
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-11-25 16:44:14 -0300

    updates

--- 02-modularizacao/app.js
+++ 02-modularizacao/app.js
@@ -3,10 +3,10 @@ import modificador from "./modificador.js";
 console.log(modificador);
 
 var ingredientes = ['mel', 'água', 'sal', 'mostarda'];
-
 var resultadoCapitalizado = modificador.capitalizar(ingredientes);
-
-var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado)
+var resultadoOrdenacao = modificador.ordenar(resultadoCapitalizado);
+var resultadoCaixaAlta = modificador.caixaAlta(ingredientes);
 
 console.log(resultadoCapitalizado);
 console.log(resultadoOrdenacao);
+console.log(resultadoCaixaAlta);
