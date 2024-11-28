48ebc1bfcf874039112033d0024601a9bf50206f
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-11-28 16:56:47 -0300

    updates

--- 05-projeto-interface-grafica/app.js
+++ 05-projeto-interface-grafica/app.js
@@ -1,2 +1,22 @@
-import ingredientes from "./ingredientes";
-import modificador from "./modificador";
+import ingredientes from "./ingredientes.js";
+import modificador from "./modificador.js";
+
+let ingredientesOrdenados = modificador.ordenar(ingredientes, 'nome');
+
+modificador.capitalizar(ingredientes, 'nome');
+
+let containerIngredientes = document.getElementById('container-ingredientes');
+
+for(let ingrediente of ingredientesOrdenados) {
+
+    let textoHTML = `
+        <div class="ingrediente">
+            <img src="img/${ingrediente.img}">
+            <p class="nome-ingrediente">${ingrediente.nome}</p>
+        </div>
+    `;
+
+    containerIngredientes.innerHTML += textoHTML;
+
+}
+
