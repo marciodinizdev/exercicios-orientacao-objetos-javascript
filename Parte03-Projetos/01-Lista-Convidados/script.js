2445e103dd2ffa3ad89c9f34a6f91250fa264c8f
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-01-23 14:35:08 -0300

    Updates

---
+++ Parte03-Projetos/01-Lista-Convidados/script.js
@@ -0,0 +1,63 @@
+/* 
+
+Aplicação que recebe nomes de convidados para uma festa e os armazena em uma lista. Ao 
+final, exibe: 
+1 -> Quantos convidados foram adicionados. 
+2 -> A lista completa de convidados. 
+3 -> Uma mensagem personalizada para cada convidado, como: "Bem-vindo, [nome]! Estamos 
+ansiosos para a festa!".
+
+*/
+
+const convidados = [];
+
+const input = document.querySelector('.input');
+const resposta = document.querySelector('.resposta');
+const listaFinal = document.querySelector('.lista-final');
+
+const tituloLista = document.createElement('h2');
+tituloLista.textContent = 'Lista de convidados';
+
+const quantidade = document.createElement('p');
+quantidade.innerHTML = 'Quantidade de convidados: ' + convidados.length;
+
+listaFinal.appendChild(tituloLista);
+listaFinal.appendChild(quantidade);
+
+function convidar() {
+
+    let nome = input.value.trim();
+    resposta.style.animation = 'none'
+
+    if (nome === '') {
+        resposta.innerHTML ='<span style="color: red; font-weight: bold" >Nome inválido!</span>' + ' Por favor, insira um nome válido!';
+        return;
+    } else {
+        convidados.push(nome);
+        input.value = '';
+        resposta.innerHTML = `Você adicionou <span style="color: #5DFE4D; font-weight: bold;">${nome}</span> à lista de convidados`;
+    }
+
+}
+
+function finalizar() {
+    listaFinal.innerHTML = '';
+    listaFinal.appendChild(tituloLista);
+    listaFinal.appendChild(quantidade);
+
+    if (convidados.length === 0) {
+        quantidade.innerHTML = 'Quantidade de convidados: ' + convidados.length + '<br><br> Ninguém foi convidado para a festa...';
+    } else {
+        quantidade.innerHTML = 'Quantidade de convidados: ' + convidados.length + '<br><br> Convidados: <br>' + convidados;
+
+        const ul = document.createElement('ol');
+        listaFinal.appendChild(ul)
+        convidados.forEach((convidado) => {
+            const li = document.createElement('li');
+            li.textContent = `Boas vindas, ${convidado}, Estamos ansiosos para a festa!`
+            ul.appendChild(li)
+        })
+
+    }
+
+}
