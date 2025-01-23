c60a8baf9aab1f327c42fef898040eb555a0173d
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-01-23 19:30:49 -0300

    Update script.js

--- Parte03-Projetos/02-Verificador-Idade/script.js
+++ Parte03-Projetos/02-Verificador-Idade/script.js
@@ -8,15 +8,15 @@ const verificar = () => {
         resultado.innerHTML = `<span style="color: red; font-weight: bold">Valor Inválido!</span> Digite uma idade válida` 
     }
     else if (idade == 0) {
-        resultado.innerHTML = 'Você não nasceu!'
+        resultado.innerHTML = 'Você não nasceu! 🥚'
     }
     else if (idade <= 12) {
-        resultado.innerHTML = 'Você é uma criança'
+        resultado.innerHTML = 'Você é uma criança 👶'
     } else if (idade > 12 && idade < 18) {
-        resultado.innerHTML = 'Você é um adolescente';
+        resultado.innerHTML = 'Você é um adolescente 🧒';
     } else if (idade >= 18 && idade < 60) {
-        resultado.innerHTML = 'Você é um adulto'
+        resultado.innerHTML = 'Você é um adulto 👨'
     } else {
-        resultado.innerHTML = 'Você é um idoso'
+        resultado.innerHTML = 'Você é um idoso 👴'
     }
 }
